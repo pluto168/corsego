94d540851be13b392fe214d44c016b52c670277c
pluto168 <86468019+pluto168@users.noreply.github.com>
2023-04-19 23:42:28 +0800

    8. hello world - generate a static page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "static_pages#landing_pages"
+  
+  #get 'static_pages/landing_pages'
+  #get 'static_pages/privacy_policy'
+
+  get "privacy_policy", to: "static_pages#privacy_policy"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routi ng.html
+
+
 end
