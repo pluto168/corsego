89e9314e5ff112f9952d0cdfa786ded1a55332f6
pluto168 <86468019+pluto168@users.noreply.github.com>
2023-04-23 00:20:53 +0800

    31. styling courses and home index

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,12 @@
 class StaticPagesController < ApplicationController
+  skip_before_action :authenticate_user!, :only => [:landing_pages]
+
   def landing_pages
+    @courses = Course.all.limit(3)
+    @latest_couses = Course.all.limit(3).order(created_at: :desc)
   end
 
   def privacy_policy
   end
+
 end
