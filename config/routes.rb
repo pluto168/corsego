95d12b4389a720a0318c8cc319eb9139efe4af30
pluto168 <86468019+pluto168@users.noreply.github.com>
2023-04-23 21:42:56 +0800

    34. devise trackable (track user IP address, create users index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :courses
-  root "static_pages#landing_pages"
+  resources :users
+
   
   #get 'static_pages/landing_pages'
   #get 'static_pages/privacy_policy'
-
+  
   get "privacy_policy", to: "static_pages#privacy_policy"
+  root "static_pages#landing_pages"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routi ng.html
 
 
