1f848c3d1812bd0cb2e58b0403e6d01fc45a9f73
pluto168 <86468019+pluto168@users.noreply.github.com>
2023-04-27 21:42:35 +0800

    49. gem rolify - add roles to users

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem 'devise', '~> 4.9', '>= 4.9.2'
 gem 'friendly_id', '~> 5.5'
 gem 'ransack', '~> 3.2', '>= 3.2.1'
 gem 'public_activity', '~> 2.0', '>= 2.0.2'
+gem 'rolify', '~> 6.0', '>= 6.0.1'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,18 +2,38 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable, :confirmable
-
+  :recoverable, :rememberable, :validatable, :trackable, :confirmable
+  
   def to_s
     email
   end
-
+  
   #user email split@maiil.com
   def username
     # if email.present?
-      self.email.split(/@/).first
+    self.email.split(/@/).first
     # end
   end
-
+  
   has_many :courses
+  
+  #
+  rolify
+  after_create :assign_default_role
+
+  # def assign_default_role
+  #   self.add_role(:student) if self.roles.blank?
+  # end
+
+  def assign_default_role
+    if User.count == 1
+      self.add_role(:admin) if self.roles.blank?
+      self.add_role(:teacher)
+      self.add_role(:student)
+    else
+      self.add_role(:student) if self.roles.blank?
+      self.add_role(:teacher) #if you want any user to be able to create own courses
+    end
+  end
+  
 end
