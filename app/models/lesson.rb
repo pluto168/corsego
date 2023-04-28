94909f1ee98fe46278d9a7268934dbec017b7786
pluto168 <86468019+pluto168@users.noreply.github.com>
2023-04-28 23:01:55 +0800

    59. public_activity for Lessons, create lessons inside courses

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -3,6 +3,18 @@ class Lesson < ApplicationRecord
 
   validates :title, :content, :course, presence: true
 
+  #
   extend FriendlyId
   friendly_id :title, use: :slugged
+
+
+  #
+  include PublicActivity::Model
+  tracked owner: Proc.new{ |controller, model| controller.current_user }
+
+  def to_s
+    title
+  end
+
+  
 end
